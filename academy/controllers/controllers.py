6de4b0ac8203054c6af85048e7020757da900245
david <rosales9146@gmail.com>
2020-06-30 12:39:06 -0500

    [REF] Templates

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -4,7 +4,9 @@ from odoo import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
-        return "Hello, world"
+        return http.request.render('academy.index', {
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
 
 #     @http.route('/academy/academy/objects/', auth='public')
 #     def list(self, **kw):
