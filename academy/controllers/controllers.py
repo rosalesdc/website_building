e4f6297de31c0882945072db01c0784b4b15e164
david <rosales9146@gmail.com>
2020-06-30 14:11:33 -0500

    [REF] Website support -  create a generic page for teacher biographies

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -10,9 +10,11 @@ class Academy(http.Controller):
             'teachers': Teachers.search([])
         })
 
-    @http.route('/academy/<int:id>/', auth='public', website=True)
-    def teacher(self, id):
-        return '<h1>{}({})</h1>'.format(id, type(id).__name__)
+    @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('academy.biography', {
+            'person': teacher
+        })
 
 #     @http.route('/academy/academy/objects/', auth='public')
 #     def list(self, **kw):
