644e76ca5a70e9f9d82600baf7395e6700d6d436
david <rosales9146@gmail.com>
2020-06-30 12:47:49 -0500

    [REF] Defining the data model

--- academy/models/models.py
+++ academy/models/models.py
@@ -2,14 +2,7 @@
 
 from odoo import models, fields, api
 
-# class academy(models.Model):
-#     _name = 'academy.academy'
+class Teachers(models.Model):
+    _name = 'academy.teachers'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
