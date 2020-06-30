88044083e72c2f8a0556132579b951008212f840
david <rosales9146@gmail.com>
2020-06-30 13:14:41 -0500

    [REF] Accessing the data

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -4,8 +4,9 @@ from odoo import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
+        Teachers = http.request.env['academy.teachers']
         return http.request.render('academy.index', {
-            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+            'teachers': Teachers.search([])
         })
 
 #     @http.route('/academy/academy/objects/', auth='public')
